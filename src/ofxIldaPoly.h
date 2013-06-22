f3cf66becb74cef2673c240874eb7056b6281707
Omer Shapira <info@omershapira.com>
2013-06-21 21:46:14 -0400

    Adds a greedy sorting algorithm and optimization params, also adds a reversal for the polylines

--- src/ofxIldaPoly.h
+++ src/ofxIldaPoly.h
@@ -8,50 +8,107 @@
 
 #pragma once
 
+//#include <algorithm>
+
 namespace ofxIlda {
     
 	class Poly: public ofPolyline{
     private:
         ofVec2f headDirection, tailDirection;
+        bool reversed;
+        
+        /**
+         * Returns the current head or tail, according to the direction (reversed param)
+         * @param head whether or not to return the head of the line
+         */
+        const ofPoint& getEnd(bool head){
+            bool order = head && (!reversed);
+            if (order){
+                //return head;
+                return *(getVertices().begin());
+            } else {
+                //return tail
+                return *(getVertices().end()-1);
+            }
+        }
+        /**
+         * Returns the current head or tail directions, according to the direction (reversed param)
+         * recalculates if necessary
+         * @param head whether or not to return the head of the line
+         */
+        
+        const ofVec2f& getEndDirection(bool head){
+            bool order = head && (!reversed);
+            if (order){
+                //return head
+                if (hasChanged() /* TODO: or is empty */){
+                    headDirection = *(getVertices().begin() + 1) - *getVertices().begin();
+                }
+                return headDirection;
+            } else {
+                if (hasChanged() /* TODO: or is empty */){
+                    tailDirection=(ofVec2f) (*(getVertices().end() - 1) - *(getVertices().end() - 2));
+                    tailDirection.normalize();
+                }
+                return tailDirection;
+            }
+        }
+        
+        
 	public:
+
 		ofFloatColor color;
         
-		Poly() : color(ofFloatColor(1, 1, 1, 1)) {}
+        /* Constructors */
         
-        Poly(ofFloatColor color) : color(color) {}
+		Poly() : color(ofFloatColor(1, 1, 1, 1)), reversed(false) {}
         
-        Poly(const Poly& poly) : ofPolyline(poly), color(poly.color) {}
+        Poly(ofFloatColor color) : color(color), reversed(false) {}
         
-        Poly(const ofPolyline& polyline) : ofPolyline(polyline), color(ofFloatColor(1, 1, 1, 1)) {}
+        Poly(const Poly& poly) : ofPolyline(poly), color(poly.color), reversed(false) {}
         
-        Poly(const ofPolyline& polyline, ofFloatColor color) : ofPolyline(polyline), color(color) {}
+        Poly(const ofPolyline& polyline) : ofPolyline(polyline), color(ofFloatColor(1, 1, 1, 1)), reversed(false) {}
         
-		Poly(const vector<ofPoint>& verts) : ofPolyline(verts), color(ofFloatColor(1, 1, 1, 1)) {}
+        Poly(const ofPolyline& polyline, ofFloatColor color) : ofPolyline(polyline), color(color), reversed(false) {}
         
-		Poly(const vector<ofPoint>& verts, ofFloatColor color) : ofPolyline(verts), color(color) {}
+		Poly(const vector<ofPoint>& verts) : ofPolyline(verts), color(ofFloatColor(1, 1, 1, 1)), reversed(false) {}
         
-        void setFromPolyline(const ofPolyline& polyline) { ofFloatColor tmpColor = color; *this = polyline; color = tmpColor; }
+		Poly(const vector<ofPoint>& verts, ofFloatColor color) : ofPolyline(verts), color(color), reversed(false) {}
+        
+        void setFromPolyline(const ofPolyline& polyline) {
+            ofFloatColor tmpColor = color;
+            *this = polyline;
+            color = tmpColor;
+            reversed = false;
+        }
+        
+        /* Getters and Setters */
         
         const ofPoint& getHead(){
-            return *(getVertices().begin());
+            getEnd(true);
         }
         const ofPoint& getTail(){
-            return *(getVertices().end()-1);
+            getEnd(false);
         }
         
+        bool const isReversed(){ return reversed; }
+        void reverse(){ reversed^=reversed; }
+        
         const ofVec2f& getHeadDirection(){
-            if (hasChanged() /* TODO: or is empty */){
-                headDirection = *(getVertices().begin() + 1) - *getVertices().begin();
-            }
-            return headDirection;
+            return getEndDirection(true);
         }
         
         const ofVec2f& getTailDirection(){
-            if (hasChanged() /* TODO: or is empty */){
-                tailDirection=(ofVec2f) (*(getVertices().end() - 1) - *(getVertices().end() - 2));
-                tailDirection.normalize();
+            return getEndDirection(false);
+        }
+        
+        /* Mutators */
+        
+        void conformDirection() {
+            if (reversed){
+                //TODO: see if this leads to a bad access
+                std::reverse(getVertices().begin(), getVertices().end());
             }
-            return tailDirection;
         }
     };
 }
--- src/ofxIldaPolyProcessor.h
+++ src/ofxIldaPolyProcessor.h
@@ -42,6 +42,10 @@ namespace ofxIlda {
             params.collapse = 0;
             params.targetPointCount = 500;
             params.spacing = 0;
+            
+            //TODO: Acutally balance between the two when they're both implemented
+            optimizationParams.angleWeight = 0;
+            optimizationParams.distanceWeight = 1;
         }
         
         //--------------------------------------------------------------
@@ -55,14 +59,21 @@ namespace ofxIlda {
             s << "spacing : " << params.spacing << endl;
             return s.str();
         }
-
+        
         //--------------------------------------------------------------
         void update(const vector<Poly> &origPolys, vector<Poly> &processedPolys) {
             float totalLength = 0;
             vector<int> pathLengths;
             processedPolys = origPolys;
+            //first pass: sort polys
+            sequenceBeam(processedPolys, false);
             for(int i=0; i<processedPolys.size(); i++) {
                 if(processedPolys[i].size()) {
+                    //reverse if necessary
+                    for (int i = 0; i<processedPolys.size(); i++) {
+                        processedPolys[i].conformDirection();
+                    }
+                    
                     // smooth paths
                     if(params.smoothAmount > 0) processedPolys[i].setFromPolyline(processedPolys[i].getSmoothed(params.smoothAmount));
                     
@@ -98,26 +109,52 @@ namespace ofxIlda {
         //--------------------------------------------------------------
         /**
          * Beam sequence planner.
-         * 
+         *
          * FIRST BASIC SOLUTION: Start out at current beam point, look for closest point (in or out)
          * in a different polygon
          *
-         * If shape is closed, pick the closest one.
-         * If shape is open, choose the closest point
-         *
          */
-        void sequenceBeam(vector<Poly> &processedPolys, ofPoint lastPoint, bool lookahead){
-            //Step 1: tag all polylines for "openness"
-            //Step 2: partition the space into nxn
-            //Step 3: find the closest point to lastPoint
-
+        void sequenceBeam(vector<Poly> &polys, bool lookahead, ofPoint lastPoint=ofPoint(0.5,0.5)){
+             if (!lookahead){
+                //convenience Point:
+                Poly p;
+                //Two, so a direction would exist
+                p.lineTo(lastPoint);
+                p.lineTo(lastPoint);
+                //insert conveniece point
+                polys.insert(polys.begin(), p);
+                
+                for (int i = 0; i < polys.size(); i++) {
+                    float minCost = INFINITY;
+                    float indexShift = 0;
+                    for (int j = 1; i + j < polys.size(); j++) {
+                        float c = cost(polys[i], polys[i+j], false);
+                        //If the head is closer, mark it
+                        if (c < minCost){
+                            indexShift = j;
+                            minCost = c;
+                        }
+                        c = cost(polys[i], polys[i+j], true);
+                        //if the tail is closer, set the polyline as reversed and mark it
+                        if (c < minCost){
+                            polys[i+j].reverse();
+                            indexShift = j;
+                            minCost = c;
+                        }
+                    }
+                    //found the closest one? bring it to the front
+                    std::iter_swap(polys.begin() + i + 1, polys.begin() + i + indexShift);
+                }
+                //remove convenience point
+                polys.erase(polys.begin());
+            }
         }
         
-        float cost(Poly l1, bool flipL1, Poly l2, bool flipL2){
+        float cost(Poly l1, Poly l2, bool flipL2){
             float c = 0;
             //FIXME: Assumes normalized. Add cases for normalized vs non-normalized
-            ofPoint p1 = flipL1? l1.getHead() : l1.getTail();
-            ofPoint p2 = flipL2? l2.getTail() : l2.getHead();
+            ofPoint p1 = l1.getHead();
+            ofPoint p2 = flipL2 ? l2.getTail() : l2.getHead();
             c += p1.distance(p2) * optimizationParams.distanceWeight;
             //TODO: implement angle weight with dot products
             
@@ -125,5 +162,5 @@ namespace ofxIlda {
         }
         
     };
-
+    
 }   
